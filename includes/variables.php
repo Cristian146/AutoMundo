70dacc335dec71acf57ae274b2ecb8fe67260bd8
Cristian <crislucero146@gmail.com>
2022-04-29 15:56:35 -0500

    mi primer commit

---
+++ includes/variables.php
@@ -0,0 +1,37 @@
+<?php
+	
+	$access_key = "90336";
+	
+	
+	$gplay_url = "https://play.google.com/store/apps/details?id=AutoMundo";
+	
+	$email_subject = "Notificación de cambios en la información de la cuenta!";
+	$change_message = "Has cambiado tu información de administrador, como correo electrónico o contraseña.";
+	$reset_message = "Tu nueva contraseña es ";
+	
+	
+	$reservation_subject = "Notificación de nuevo pedido!";
+	$reservation_message = "Hay un nuevo pedido, consulte el Panel de administración.";
+	$order_subject = "Pedido Confirmar notificación!";
+	$order_message = "Su orden ha sido puesta. te contactaremos muy pronto";
+	
+	$register_subject = "Gracias por registrarse en AutoMundo";
+	$register_message = "Gracias por registrarse en AutoMundo. Le damos la bienvenida a la mejor experiencia de compra para su vehiculo.";
+	$process_subject = "Correo de confirmación para su pedido de AutoMundo no ";
+	$process_message = "Gracias por comprar en AutoMUndo<br>
+	Los siguientes artículos en su pedido serán enviados desde nuestro almacén. <br>
+	Una vez que se hayan enviado estos artículos, le enviaremos un correo electrónico con la fecha de entrega esperada. <br>
+	Detalles del pedido:  <br>";
+
+
+
+		
+	$shipped_subject = " Su pedido de AutoMundo ha sido enviado desde aquí";
+	$shipped_message = "Gracias por su pedido en AutoMundo Tu pedido no .";
+						
+	$delievered_subject = " Su pedido de AutoMundo no.";
+	$delievered_message = "Gracias por su pedido en AutoMundo Tu pedido no .";
+	
+	$copyright = "";
+
+?>
